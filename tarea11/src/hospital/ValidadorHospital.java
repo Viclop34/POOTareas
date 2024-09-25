6fe2c16f143d789b5970eb346391e068f6f87865
Viclop34 <vl48011@gmail.com>
2024-09-24 22:25:23 -0600

    Tarea 11 terminada

---
+++ tarea11/src/hospital/ValidadorHospital.java
@@ -0,0 +1,26 @@
+package hospital;
+
+import consultas.Consulta;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class ValidadorHospital {
+    public boolean validarDisponibilidadMedico(LocalDateTime FechaDeseada, String idMedico, ArrayList<Consulta> listaConsultas) {
+        for(Consulta consulta : listaConsultas) {
+            if(consulta.getFechaHora().isEqual(FechaDeseada) && consulta.getMedico().getId() == idMedico) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validarDispolnibilidadEnFechaConsulta(LocalDateTime fechaDeseada, int numeroConsultorio, ArrayList<Consulta> listaConsultas) {
+        for(Consulta consulta : listaConsultas) {
+            if(consulta.getFechaHora().isEqual(fechaDeseada) && numeroConsultorio == consulta.getConsultorio().getNumeroConsultorio()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
